9d2dadf7a0b10a2b42671f9faaa075958111ed0e
Alex <nosov3006@inbox.ru>
2022-11-24 11:14:44 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
         FixPokemons Terrakion = new Terrakion("Великолепный покемон",-5);
         FixPokemons Zigzagoon = new Zigzagoon("Суир", -7);
         FixPokemons Linoone = new Linoone("Пегас", 6);
-        FixPokemons Vibrava = new Vibrava("Данила", -5);
-        FixPokemons Trapinch = new Trapinch("Саня", 8);
-        FixPokemons Flygon = new Flygon("Лаба", -7);
+        FixPokemons Vibrava = new Vibrava("Данила", 5);
+        FixPokemons Trapinch = new Trapinch("Саня", -8);
+        FixPokemons Flygon = new Flygon("Лаба", 7);
 
 
         FixPokemons[] Foe = {Terrakion, Zigzagoon, Trapinch};
